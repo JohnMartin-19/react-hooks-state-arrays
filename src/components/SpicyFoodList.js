eb901bca811c8a20a4970491e3a495b407eaeff4
John <johnnkonge2020@gmail.com>
2023-10-27 10:35:51 +0300

    added

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -23,4 +23,67 @@ function SpicyFoodList() {
   );
 }
 
+//update state and get new food dynamically
+// adding elements to array in state
+function handleAddFood(){
+  const newFood = getNewSpicyFood()
+  
+  //using spread operator to make a copy of our foods array and insert it in a new array
+  const newFoodArray = [...foods, newFood]
+  setFoods(newFoodArray);
+}
+
+
+//removing element from Arrays in State
+const foodList = foods.map((food)=>(
+  <li key={food.id} onClick={()=> handleLiClick(food.id)}>
+    {food.name} | Heat:{food.heatLevel} | Cuisine: {food.cuisine}
+    //using .filter() to remove
+ 
+  </li>
+
+))
+
+function handleLiClick(id){
+  const newFoodArray = foos.map((food)=>{
+    if(food.id === id){
+      return{
+        ...food,
+        heatLevel: food.heatLevel+1
+      }
+    } else{
+      return food
+    }
+  })
+  setFoods(newFoodArray)
+}
+
+const [filterBy, setFilterBy] =  useState('All')
+function handleFilter(event){
+  setFilterBy(event.target.value)
+  return(
+    <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
+  )
+}
+
+const [foods, setFoods] = useState(spicyFoods);
+
+
+const foodsToDisplay = foods.filter((food)=>{
+  if(filterBy === 'All'){
+    return true;
+  } else {
+    return food.cuisine === filterBy
+  }
+  
+})
+
+
+
 export default SpicyFoodList;
